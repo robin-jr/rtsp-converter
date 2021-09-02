31e2ac70c8ed7d5993956adf172b6d6a73fa90e8
Robin J <robin__j@outlook.com>
2021-09-02 13:54:22 +0530

    gets rtsp url from query params

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ app.get('/camera/feed', (req, res) => {
     // Thanks to https://stackoverflow.com/q/28946904/1954789
 
     res.header('content-type', 'video/webm');
+    // const rtsp_url="rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov"
+    const rtsp_url = req.query.rtsp_url;
+    console.log(rtsp_url)
 
-    const cmd = `ffmpeg -i rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov -c:v copy -c:a copy -bsf:v h264_mp4toannexb -maxrate 500k -f matroska -`.split(' ');
+    const cmd = `ffmpeg -i ${rtsp_url} -c:v copy -c:a copy -bsf:v h264_mp4toannexb -maxrate 500k -f matroska -`.split(' ');
 
     var child = child_process.spawn(cmd[0], cmd.splice(1), {
         stdio: ['ignore', 'pipe', process.stderr]
